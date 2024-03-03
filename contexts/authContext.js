ef0dd4ca6c1ddc63bbe79a686104c95e433120a4
Jiachen Sun <sunjiachen1110@gmail.com>
2024-03-02 16:57:12 -0800

    added authcontext and login page skeleton

---
+++ contexts/authContext.js
@@ -0,0 +1,49 @@
+import { createContext } from 'react'
+
+const AuthContext = createContext({
+    user: null,
+    login: async () => {},
+    logout: async () => {},
+    // boolean is true when connection to firebase is established
+    authReady: false,
+})
+
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [authReady, setAuthReady] = useState(false)
+
+    const login = async (email, password) => {
+        try {
+        //   await auth.signInWithEmailAndPassword(email, password)
+        } catch (error) {
+          console.error("Login failed:", error)
+        }
+      };
+    
+    const logout = async () => {
+    // await auth.signOut()
+    };
+
+    useEffect(() => {
+        // const unsubscribe = auth.onAuthStateChanged((user) => {
+        //   setUser(user)
+        //   setAuthReady(true) // Set authReady to true once we get the initial user value
+        // });
+    
+        // // Cleanup subscription on unmount
+        // return () => unsubscribe()
+      }, [])
+
+    const context = {
+        user,
+        login,
+        logout,
+        authReady
+    }
+
+    return (
+        <AuthContext.Provider value={context}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
