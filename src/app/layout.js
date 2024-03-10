a4f201dd1930960e85827081e13972505da7dd50
Jiachen Sun <sunjiachen1110@gmail.com>
2024-03-10 01:16:08 -0800

    favicon change, a lil cleanup

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <AuthContextProvider>
-      <body style={{ backgroundColor: "#282828" }} className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
       </AuthContextProvider>
     </html>
   );
