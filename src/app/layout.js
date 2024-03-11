9cc5602ac62a2c6a8636809a0e588dddaca91454
Jiachen Sun <sunjiachen1110@gmail.com>
2024-03-10 23:35:19 -0700

    made an outer mobile container

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import styles from "@/page.module.css";
 import { AuthContextProvider } from "../../contexts/authContext";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <AuthContextProvider>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className={styles.main}>
+          <div className={styles.mobileContainer}>
+            {children}
+          </div>
+        </main>
+      </body>
       </AuthContextProvider>
     </html>
   );
