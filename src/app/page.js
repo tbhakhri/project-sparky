52eb1279f9e0ac36b651c2f638754b8a34a86e99
Jiachen Sun <sunjiachen1110@gmail.com>
2024-03-11 00:42:00 -0700

    setup for home page styling

--- src/app/page.js
+++ src/app/page.js
@@ -11,24 +11,47 @@ import BottomInputBox from "@/molecules/BottomInputBox/BottomInputBox";
 export default function App() {
   const { user, logout, authReady } = useContext(AuthContext)
   return (
-      <>
+    <div className={styles.pageContainer}>
       {
         authReady ? <>
         {user === null ? redirect('/login') : (
           <>
-            <main className={styles.main}>
+            <>
               MAIN PAGE
               <CompareRerun onParameterChange={() => {}}></CompareRerun>
               <BottomInputBox />
               <PutRunSettings />
-            </main>
+            </>
+            {/* TODO: MOVE THIS INTO THE TOP BAR MAYBE */}
             <button onClick={() => logout()}>Logout</button>
           </>
+          /*
+          Idea is that instead of:
+          <>
+            <>
+              MAIN PAGE
+              <CompareRerun onParameterChange={() => {}}></CompareRerun>
+              <BottomInputBox />
+              <PutRunSettings />
+            </>
+            <button onClick={() => logout()}>Logout</button>
+          </>
+
+          we'll have:
+
+          <TopMenu>
+          <MainContent> naming TBD
+          <BottomMenu>
+
+          CONSIDERATIONS:
+          - How much space should TopMenu, MainContent, and BottomMenu each take up?
+          - Depending on how/whether the text box in BottomMenu resizes as you type text in, will the height of the BottomMenu change? If so we need to thoroughly test it to make sure nothing looks wonky
+          */
         )}
         </> : <div>
           Loading...
         </div>
       }
-      </>
+    </div>
   )
 }
