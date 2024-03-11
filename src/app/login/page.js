08ac2c0f42c208c6fde1b881acd36d771ce1ff1e
Jiachen Sun <sunjiachen1110@gmail.com>
2024-03-11 00:10:48 -0700

    styling for login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,30 +1,36 @@
-"use client";
+"use client"
 
-import { useContext } from 'react';
-import AuthContext from '../../../contexts/authContext';
-import GoogleSignInButton from './logo';
-import './page.css';
+import { useContext } from 'react'
+import AuthContext from '../../../contexts/authContext'
+import GoogleSignInButton from './logo'
+import './page.css'
+import styles from "@/page.module.css"
 import { redirect } from 'next/navigation'
+import Image from 'next/image'
 
 export default function Login() {
 
   const { user, login, authReady } = useContext(AuthContext);
 
   return (
-    <>
+    <div className={styles.pageContainer}>
     {
       authReady ? <div>
       {
         user === null ? 
-        <GoogleSignInButton onClick={() => login()} />
+        <div className="outerContainer">
+          <Image src="/sparkyLogo.svg" alt="sparky logo" width={230} height={100} style={{paddingBottom: "30px"}}/>
+          <GoogleSignInButton onClick={() => login()} />
+
+        </div>
         :
         redirect('/')
       }
       </div> : <div>
         Loading...
-    </div>
+      </div>
     }
-    </>
+    </div>
   );
 }
 
