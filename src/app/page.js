a404f05b5e50c44dc186a9c27bb4bceff5d3d3fc
Jiachen Sun <sunjiachen1110@gmail.com>
2024-03-11 19:06:22 -0700

    set up some initial state for prompt screen

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 import AuthContext from "../../contexts/authContext"; 
 import styles from "@/page.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { redirect } from "next/navigation";
 import CompareRerun from "@/molecules/CompareRerun/CompareRerun";
 import PutRunSettings from "@/molecules/PutRunSettings/PutRunSettings";
@@ -10,6 +10,46 @@ import BottomInputBox from "@/molecules/BottomInputBox/BottomInputBox";
 
 export default function App() {
   const { user, logout, authReady } = useContext(AuthContext)
+  
+  const [promptMetadata, setPromptMetadata] = useState(() => ({
+    id: generateUUID(),
+    name: 'My Prompt',
+    lastModified: new Date(),
+    requestChain: [],
+    responses: [],
+  }));
+  const [curModelSettings, setCurModelSettings] = useState({
+    modelName: 'Gemini 1.0 Pro Vision',
+    temperature: 0.4,
+    topP: 1,
+    topK: 32,
+  })
+  
+  const tokenCount = calculateTokenCount(promptMetadata.requestChain)
+  const maxTokenCount = calculateMaxTokenCount(curModelSettings.modelName)
+
+  function generateUUID() {
+    const buffer = crypto.getRandomValues(new Uint8Array(4))
+    return buffer
+      .toString('base64')
+      .replace(/,/g, '')
+  }  
+
+  function calculateTokenCount(requestChain) {
+    // TODO: REPLACE WITH WORKING IMPLEMENTATION
+    return 1000
+  }
+
+  function calculateMaxTokenCount(modelName) {
+    switch (modelName) {
+      case 'Gemini 1.0 Pro':
+        return 30720
+      case 'Gemini 1.0 Pro Vision':
+        return 12288
+      default: return 10000
+    }
+  }
+
   return (
     <div className={styles.pageContainer}>
       {
