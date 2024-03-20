976b7f0171164d9804ced84077669cfca7c409d3
Jiachen Sun <sunjiachen1110@gmail.com>
2024-03-19 23:02:31 -0700

    added edit and delete buttons

--- src/app/molecules/ChatBubble/ChatBubble.js
+++ src/app/molecules/ChatBubble/ChatBubble.js
@@ -1,13 +1,25 @@
 import { Kumbh_Sans } from "next/font/google"
 import "./ChatBubble.css"
+import Image from "next/image"
 
 const kumbh_sans = Kumbh_Sans({ subsets: ["latin"] })
 
 export default function ChatBubble({ text }) {
   return (
     <div className="bubble-container">
+      <button className="editDeleteButton">
+        <Image src="/edit.svg" alt="Edit text" width={10} height={10} />
+      </button>
+      <button className="editDeleteButton">
+        <Image
+          src="/x-button-white.svg"
+          alt="Delete text"
+          width={10}
+          height={10}
+        />
+      </button>
+      <></>
       <div className={kumbh_sans.className}>
-        {/* {index == 0 && <p className="userText">User</p>} */}
         <p className="chatBubble">{text}</p>
       </div>
     </div>
