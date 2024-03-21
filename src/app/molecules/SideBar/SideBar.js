63400ae1bd42a535dad65fe6c0da9ebe36ea6b26
Jessica He <jjhe9999@berkeley.edu>
2024-03-21 13:05:56 -0700

    styled like figma now

--- src/app/molecules/SideBar/SideBar.js
+++ src/app/molecules/SideBar/SideBar.js
@@ -4,28 +4,50 @@ import Image from "next/image";
 export default function SideBar({ toggleSidebar }) {
   return (
     <div className="sideBarContainer">
-      <button className="historyButton" onClick={toggleSidebar}>
+      {/* <button className="historyButton" onClick={toggleSidebar}>
         <Image
           src="/history-button.svg"
           alt="historyButton"
           width={20}
           height={20}
         />
-      </button>
+      </button> */}
       <div className="menuBarContainer">
-        helllllllo
-        <div className="menuBarItem">?</div>
-        <div className="menuBarItem">Menu</div>
-        <div className="menuBarItem">L</div>
+        <div className="menuBarItem">
+          <Image src="/help-button.svg" alt="help" width={25} height={25} style={{margin: '-2px'}}/>
+          {/* could give className=icon */}
+        </div>
+        <div className="menuBarItem">
+          <div className="menuBarText">
+            Menu
+          </div>
+        </div>
+        <div className="menuBarItem">
+        <button className="closeMenuButton" onClick={toggleSidebar}>
+            <Image src="/close-menu.svg" alt="help" width={17} height={17} style={{margin: '2px'}}/>
+          </button>
+        </div>
+      </div>
+      <div className="newPromptButtonContainer">
+        <button className="newPromptButton" onclick={toggleSidebar}>
+          New Prompt 
+        </button>
       </div>
       <div className="currentPromptContainer">
-        <div className="promptItem"> insert prompt here</div>
+        <div className="promptHeaderText">
+          Current Prompt
+        </div>
+        <div className="promptItem"> Insert prompt here</div>
       </div>
+
       <div className="pastPromptContainer">
-        <div className="promptItem"> insert prompt here</div>
-        <div className="promptItem"> insert prompt here</div>
-      </div>
-      <div>Hello</div>
+      <div className="promptHeaderText">
+          Past Prompts
+        </div>
+        <div className="promptItem"> Insert prompt here</div>
+        <div className="promptItem"> Insert prompt here</div>
+        <div className="promptItem"> Insert prompt here</div>
+      </div>  
     </div>
   );
 }
