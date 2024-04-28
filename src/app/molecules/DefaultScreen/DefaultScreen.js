e86b0ff7c2909c36f27cb7df85b9f2b6442055e3
Michelle Park <michelle.taeeun@berkeley.edu>
2024-04-27 17:25:42 -0700

    Done with default inputs

--- src/app/molecules/DefaultScreen/DefaultScreen.js
+++ src/app/molecules/DefaultScreen/DefaultScreen.js
@@ -8,6 +8,7 @@ export default function DefaultScreen() {
       <div className="ideaBoxContainer">
         <div className="ideaBoxItem">
           <IdeaBox
+            id="0"
             text="Write a story about a magic backpack"
             pathToSvg="/textbox.svg"
             alt="hi"
@@ -15,6 +16,7 @@ export default function DefaultScreen() {
         </div>
         <div className="ideaBoxItem">
           <IdeaBox
+            id="1"
             text="Take a photo of your food and ask me for the recipe"
             pathToSvg="/camerafordefaultscreen.svg"
             alt="bye"
@@ -22,12 +24,14 @@ export default function DefaultScreen() {
         </div>
         <div className="ideaBoxItem">
           <IdeaBox
+            id="2"
             text="Hum a tune and ask me to identify the song"
             pathToSvg="/audio.svg"
           />
         </div>
         <div className="ideaBoxItem">
           <IdeaBox
+            id="3"
             text="Insert an image and ask me anything about it"
             pathToSvg="/landscape.svg"
             alt="hi"
